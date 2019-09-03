ccbfb7cf25096b96dbfcc0ef318f8dd615c57b5c
Emmanuel M <emmanuelbendavid@gmail.com>
2019-09-03 09:42:31 -0300

    Remove comments of main

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'business_search.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,15 +25,6 @@ class MyApp extends StatelessWidget {
 class IntroPage extends StatefulWidget {
   IntroPage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,12 +34,6 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: CustomPaint(
         painter: _SlashedBackground(),
